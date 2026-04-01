5b5443a0ef95ce47240fb6e0fed3993f80f2e4ec
VestaKK <139874459+VestaKK@users.noreply.github.com>
2026-04-01 19:51:44 +1100

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) {
+            return 0;
+        }
+        int lowest = prices[0];
+        int max_profit=0;
+        for (int price : prices) {
+            max_profit = max(max_profit, price - lowest);
+            lowest = min(lowest, price);
+        }
+
+        return max_profit;
+    }
+};
